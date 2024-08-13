4ff5573907e36c83e00fa3563eba565a938071fd
hassan4702 <muhammadhassanshakil35066@gmail.com>
2024-08-13 11:15:11 +0500

    input comp

---
+++ components/Input.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Input = ({ type, className, placeholder }) => {
+  return (
+    <div>
+      <input type={type} className={className} placeholder={placeholder} />
+    </div>
+  );
+};
+
+export default Input;
