71c9359d8b222add49ca20f17d915d9524ba134b
hassan4702 <muhammadhassanshakil35066@gmail.com>
2024-08-13 10:55:23 +0500

    First commit with submod

---
+++ components/Button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Button = ({ className, text, type }) => {
+  return (
+    <div>
+      <button type={type} className={className}>
+        {text}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
